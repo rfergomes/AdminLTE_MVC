645b5c011f91682c565c7c3aead06f3c29cfa7eb
Rodrigo Fernando Gomes Lima <31287854+rfergomes@users.noreply.github.com>
2021-07-27 20:26:00 -0300

    EmailService

--- app/models/service/EmailService.php
+++ app/models/service/EmailService.php
@@ -8,75 +8,42 @@ use stdClass;
 
 class EmailService
 {
-    /** @var PHPMailer */
-    private $mail;
-
-    /** @var stdClass */
-    private $data;
-
-    /** @var Exception */
-    private $error;
-
-    public function __construct()
+    public static function enviar($email, $msg, $assunto)
     {
-        $this->mail = new PHPMailer(true);
-        $this->data = new stdClass();
+        $mail = new PHPMailer(true);
 
-        $this->mail->isSMTP();
-        $this->mail->isHTML();
-        $this->mail->setLanguage("br");
-
-        $this->mail->SMTPAuth = true;
-        $this->mail->SMTPSecure = CONF_MAIL["MODO"];
-        $this->mail->CharSet = "utf-8";
-
-        $this->mail->Host = CONF_MAIL["HOST"];
-        $this->mail->Port = CONF_MAIL["PORTA"];
-        $this->mail->Username = CONF_MAIL["USUARIO"];
-        $this->mail->Password = CONF_MAIL["SENHA"];
-    }
-
-    public function email(string $subject, string $body, string $recipient_name, string $recipient_email): EmailService
-    {
-        
-        $this->data->subject = $subject;
-        $this->data->body = $body;
-        $this->data->recipient_name = $recipient_name;
-        $this->data->recipient_email = $recipient_email;
-        return $this;
-    }
-
-    public function anexo(string $filePath, string $fileName): EmailService
-    {
-        
-        $this->data->attach[$filePath] = $fileName;
-        return $this;
-    }
-
-    public function send(string $from_name = CONF_MAIL["REMETENTE"], string $from_email = CONF_MAIL["EMAIL"]): bool
-    {
         try {
-            $this->mail->Subject = $this->data->subject;
-            $this->mail->msgHTML($this->data->body);
-            $this->mail->addAddress($this->data->recipient_email, $this->data->recipient_name);
-            $this->mail->setFrom($from_email, $from_name);
-
-            if (!empty($this->data->attach)) {
-                foreach ($this->data->attach as $path => $name) {
-                    $this->mail->addAttachment($path, $name);
-                }
-            }
-
-            $this->mail->send();
-            return true;
-        } catch (Exception $exception) {
-            $this->error = $exception;
-            return false;
+            //Server settings
+            $mail->isSMTP();                                            //Send using SMTP
+            $mail->Host       = CONF_MAIL["HOST"];                     //Set the SMTP server to send through
+            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+            $mail->Username   = CONF_MAIL["USUARIO"];                     //SMTP username
+            $mail->Password   = CONF_MAIL["SENHA"];                               //SMTP password
+            $mail->SMTPSecure = CONF_MAIL["MODO"];            //Enable implicit TLS encryption
+            $mail->Port       = CONF_MAIL["PORTA"];                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        
+            //Recipients
+            $mail->setFrom(CONF_MAIL["EMAIL"], CONF_MAIL["REMETENTE"]);
+            $mail->addAddress($email, 'Rodrigo Lima');     //Add a recipient
+            //$mail->addAddress('ti@quimicosunificados.com.br');               //Name is optional
+            //$mail->addReplyTo('quimicos.backup@gmail.com', 'TI');
+            //$mail->addCC('cc@example.com');
+            //$mail->addBCC('bcc@example.com');
+        
+            //Attachments
+            //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+            //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+        
+            //Content
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject = $assunto;
+            $mail->Body    = $msg;
+            $mail->AltBody = $msg;
+        print_r($mail);
+            $mail->send();
+            echo 'E-mail enviado!';
+        } catch (Exception $e) {
+            echo "Não foi possível enviar o e-mail. {$mail->ErrorInfo}<br><pre>". print_r($e);
         }
-    }
-
-    public function error(): ?Exception
-    {
-        return $this->error;
     }
 }
