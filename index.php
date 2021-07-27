56c1e13bdca301fb3482f14f344806242dba01b0
Rodrigo Fernando Gomes Lima <31287854+rfergomes@users.noreply.github.com>
2021-07-27 13:31:18 -0300

    Checagem .htaccess
    Verifica se o arquivo .htacces existe, se não existir gera um novo.

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once 'app/helper/helper.php';
 require_once 'app/helper/datahora.php';
 require_once 'app/helper/numero.php';
 require_once 'app/helper/rede.php';
-require_once 'config/config.php';
+//require_once 'config/config.php';
 require_once 'vendor/autoload.php';
 date_default_timezone_set(TIMEZONE);
 
