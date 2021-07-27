ff9781999d88928e463b4fd9ba4250d196482c96
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2021-07-27 20:23:31 -0300

    Atualização

--- app/views/cabecalho.php
+++ app/views/cabecalho.php
@@ -58,7 +58,7 @@
                     Brad Diesel
                     <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
                   </h3>
-                  <p class="text-sm">Call me whenever you can...</p>
+                  <p class="text-sm">Call me whenever you can... </p>
                   <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                 </div>
               </div>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use app\core\Excecao;
 session_start();
 error_reporting(E_ALL & ~E_NOTICE);
 
-require_once __DIR__ . 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set(TIMEZONE);
 
 try {
@@ -20,7 +20,7 @@ try {
 }
 
 if (!file_exists('.htaccess')) {
-    $htaccesswrite = "RewriteEngine On\r\nOptions All -Indexes\r\n\r\n# Redirecionamento WWW\r\nRewriteCond %{HTTP_HOST} !^www\. [NC]\r\nRewriteRule ^ %{ENV:PROTO}://www.%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\r\n\r\n# Redirecionamento HTTPS\r\nRewriteCond %{HTTP:X-Forwarded-Proto} !https\r\nRewriteCond %{HTTPS} off\r\nRewriteRule ^ %{ENV:PROTO}://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\r\n\r\n# Reescrevendo URL\r\nRewriteCond %{REQUEST_FILENAME} !-f\r\nRewriteCond %{REQUEST_FILENAME} !-d\r\nRewriteRule ^(.*)$ index.php/$1 [QSA,L]";
+    $htaccesswrite = "RewriteEngine On\r\nOptions All -Indexes\r\n\r\n# Redirecionamento WWW\r\nRewriteCond %{HTTP_HOST} !^www\. [NC]\r\nRewriteRule ^ https://www.%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\r\n\r\n# Redirecionamento HTTPS\r\nRewriteCond %{HTTPS} off\r\nRewriteRule ^ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\r\n\r\n# Reescrevendo URL\r\nRewriteCond %{REQUEST_FILENAME} !-f\r\nRewriteCond %{REQUEST_FILENAME} !-d\r\nRewriteRule ^(.*)$ index.php/$1 [QSA,L]";
     $htaccess = fopen('.htaccess', "w");
     fwrite($htaccess, str_replace("'", '"', $htaccesswrite));
     fclose($htaccess);
