a6bbea9fa206e2c6991f7bfd329f4e7b6ed57850
Rodrigo Fernando Gomes Lima <31287854+rfergomes@users.noreply.github.com>
2021-07-27 20:21:34 -0300

    Login/esqueci

--- app/views/Login/esqueci.php
+++ app/views/Login/esqueci.php
@@ -9,9 +9,9 @@
     </div>
     <div class="card-body">
       <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
-      <form action="recover-password.html" method="post">
+      <form action="<?= URL_BASE ?>login/recover" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="email" name="email" class="form-control" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
