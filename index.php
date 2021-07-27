3c965916ff4712ba9f385c0d3e2d2174a9f5f5f2
Rodrigo Fernando Gomes Lima <31287854+rfergomes@users.noreply.github.com>
2021-07-27 13:21:29 -0300

    .htaccess atualizado

--- index.php
+++ index.php
@@ -13,6 +13,13 @@ require_once 'config/config.php';
 require_once 'vendor/autoload.php';
 date_default_timezone_set(TIMEZONE);
 
+if (!file_exists('.htaccess')) {
+    $htaccesswrite = "RewriteEngine On\r\nOptions All -Indexes\r\n\r\n# Redirecionamento WWW\r\nRewriteCond %{HTTP_HOST} !^www\. [NC]\r\nRewriteRule ^ %{ENV:PROTO}://www.%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\r\n\r\n# Redirecionamento HTTPS\r\nRewriteCond %{HTTP:X-Forwarded-Proto} !https\r\nRewriteCond %{HTTPS} off\r\nRewriteRule ^ %{ENV:PROTO}://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\r\n\r\n# Reescrevendo URL\r\nRewriteCond %{REQUEST_FILENAME} !-f\r\nRewriteCond %{REQUEST_FILENAME} !-d\r\nRewriteRule ^(.*)$ index.php/$1 [QSA,L]";
+    $htaccess = fopen('.htaccess', "w");
+    fwrite($htaccess, str_replace("'", '"', $htaccesswrite));
+    fclose($htaccess);
+}
+
 try {
     $core = new Core;
     $core->run();
