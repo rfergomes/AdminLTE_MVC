0649718382a5cf070356577642dab79a8c0f974f
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2021-07-28 15:43:39 -0300

    Conexao atualizado

--- app/core/Conexao.php
+++ app/core/Conexao.php
@@ -13,10 +13,8 @@ class Conexao{
                 self::$conexao = new \PDO("mysql:dbname=".CONF_BD["BANCO"].";host=".CONF_BD["SERVIDOR"],CONF_BD["USUARIO"],CONF_BD["SENHA"]);
                 self::$conexao->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
                 self::$conexao->exec("SET NAMES " . CONF_BD["CHARSET"]);
-            }
-            
-            return self::$conexao;
-            
+            }           
+            return self::$conexao; 
         }catch (PDOException $e){
             throw new Exception("Erro ao tentar conectar com o banco.<br/>".$e->getMessage());
         }
