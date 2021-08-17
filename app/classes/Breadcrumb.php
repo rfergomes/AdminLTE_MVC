706e738519aa9565118689407aa024819ac0fa26
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2021-08-17 17:34:17 -0300

    Breadcrumb Atualizado

--- app/classes/Breadcrumb.php
+++ app/classes/Breadcrumb.php
@@ -5,26 +5,30 @@ namespace app\classes;
 class Breadcrumb {
 
     #Cria os breadcrumbs do site
-    public function addBreadcrumb() {
-        $Contador = count($this->parseUrl());
-        $ArrayLink[0] = '';
-        $link = "<ol class='breadcrumb float-sm-right'>";
-        for ($I = 0; $I < $Contador; $I++) {
-            $ArrayLink[0] .= $this->parseUrl()[$I];
-
-            if($Contador - 1 <= 0){
-                $link .= "<li class='breadcrumb-item'><a href='#'>Home</a></li>";
-            }elseif ($I < $Contador - 1) {
-                $link .= "<li class='breadcrumb-item active'><a href='" . URL_BASE . $ArrayLink[0] . "'>" . ucfirst($this->parseUrl()[$I]) . "</a></li>";
-            }else{
-                $link .= "<li class='breadcrumb-item active'>". ucfirst($this->parseUrl()[$I])."</li>";
-            }  
+    function addBreadcrumb($separator = '', $home = 'Home') {
+        // This gets the REQUEST_URI (/path/to/file.php), splits the string (using '/') into an array, and then filters out any empty values
+        $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
+        array_shift($path);
+        // Initialize a temporary array with our breadcrumbs. (starting with our home page, which I'm assuming will be the base URL)
+        $breadcrumbs = Array("<ol class='breadcrumb float-sm-right'><li class='breadcrumb-item'><a href='". URL_BASE ."'>$home</a></li>");
+     
+        // Find out the index for the last value in our path array
+        $last = end(array_keys($path));
+     
+        // Build the rest of the breadcrumbs
+        foreach ($path AS $x => $crumb) {
+            // Our "title" is the text that will be displayed (strip out .php and turn '_' into a space)
+            $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
+     
+            // If we are not on the last index, then display an <a> tag
+            if ($x != $last)
+                $breadcrumbs[] = "<li class='breadcrumb-item'><a href='". URL_BASE.$crumb."'> $title </a></li>";
+            // Otherwise, just display the title (minus)
+            else
+                $breadcrumbs[] = "<li class='breadcrumb-item active'>". $title ."</li></ol>";
         }
-        $link .= "</ol> ";
-        return $link;
-    }
-
-    private function parseUrl() {
-        return explode("/", rtrim($_GET['url']), FILTER_SANITIZE_URL);
+     
+        // Build our temporary array (pieces of bread) into one big string :)
+        return implode($separator, $breadcrumbs);
     }
 }
