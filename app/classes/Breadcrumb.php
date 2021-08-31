f1ed755aea37429d7b1dd7c18cc23915c34f3e55
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2021-08-31 20:54:20 -0300

    Atualizado

--- app/classes/Breadcrumb.php
+++ app/classes/Breadcrumb.php
@@ -10,16 +10,19 @@ class Breadcrumb {
         $path = array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)));
         array_shift($path);
         // Initialize a temporary array with our breadcrumbs. (starting with our home page, which I'm assuming will be the base URL)
-        $breadcrumbs = Array("<ol class='breadcrumb float-sm-right'><li class='breadcrumb-item'><a href='". URL_BASE ."'>$home</a></li>");
+        $breadcrumbs = Array("<ol class='breadcrumb float-sm-right'>");
      
         // Find out the index for the last value in our path array
         $last = end(array_keys($path));
-     
+  
         // Build the rest of the breadcrumbs
         foreach ($path AS $x => $crumb) {
             // Our "title" is the text that will be displayed (strip out .php and turn '_' into a space)
             $title = ucwords(str_replace(Array('.php', '_'), Array('', ' '), $crumb));
-     
+            echo ucwords($path[0])." | " .ucwords($crumb);
+            if(ucwords($path[0]) != ucwords($crumb) && $x == 0){
+                $breadcrumbs[] = "<li class='breadcrumb-item'><a href='". URL_BASE ."'>$home</a></li>";
+            }
             // If we are not on the last index, then display an <a> tag
             if ($x != $last)
                 $breadcrumbs[] = "<li class='breadcrumb-item'><a href='". URL_BASE.$crumb."'> $title </a></li>";
@@ -27,7 +30,7 @@ class Breadcrumb {
             else
                 $breadcrumbs[] = "<li class='breadcrumb-item active'>". $title ."</li></ol>";
         }
-     
+        
         // Build our temporary array (pieces of bread) into one big string :)
         return implode($separator, $breadcrumbs);
     }
